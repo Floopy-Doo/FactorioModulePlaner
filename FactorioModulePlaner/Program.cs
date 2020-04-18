dd7680ee5098a81dab65c187d16105b56f92a6e0
Florian Bucher <florian.bucher@bbv.ch>
2020-04-18 15:09:23 +0200

    Add project files.

---
+++ FactorioModulePlaner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactorioModulePlaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
